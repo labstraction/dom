8d351e87646920626f3d95e702c5a623a00cd0fc
labstraction <labstraction@protonmail.com>
2025-11-25 12:53:14 +0100

    fruits

--- script.js
+++ script.js
@@ -55,6 +55,80 @@ newP.innerHTML = '<strong>quarto paragrafo</strong>'
 
 
 //esempio
-
 const student1 = {name: 'alexander', surname:'macias', age: 26, gender:'male'};
 const student2 = {name: 'evelyn', surname:'medina', age: undefined, gender:'female'};
+
+//append
+const emptyDiv = document.getElementById('empty-div');
+
+const cardDiv = document.createElement('div');
+cardDiv.id = 'first-card';
+emptyDiv.appendChild(cardDiv);
+
+const nameP = document.createElement('p');
+const nameNode = document.createTextNode('nome: ' + student1.name);
+nameP.appendChild(nameNode);
+cardDiv.appendChild(nameP);
+
+const surnameP = document.createElement('p');
+const surnameNode = document.createTextNode('cognome: ' + student1.surname);
+surnameP.appendChild(surnameNode);
+cardDiv.appendChild(surnameP);
+
+const ageP = document.createElement('p');
+ageP.appendChild(document.createTextNode('età: ' + student1.age));
+cardDiv.appendChild(ageP);
+
+const genderP = document.createElement('p');
+genderP.appendChild(document.createTextNode('gender: ' + student1.gender));
+cardDiv.appendChild(genderP);
+
+
+//innerHTML
+
+const htmlString = `<div class="card">
+    <p>nome: ${student2.name}</p>
+    <p>cognome: ${student2.surname}</p>
+    <p>età: ${student2.age}</p>
+    <p>genere: ${student2.gender}</p>
+</div>`
+
+console.log(htmlString);
+console.log(emptyDiv.innerHTML);
+
+// emptyDiv.innerHTML = emptyDiv.innerHTML + htmlString;
+emptyDiv.innerHTML += htmlString;
+
+
+/// AGGIUNGERE CLASSI
+
+const card = document.getElementById('first-card');
+// card.className = 'card';
+// card.className = 'card green-background'
+
+card.classList.add('card');
+card.classList.add('green-background');
+card.classList.toggle('card');
+
+
+const fruits = [
+    {name: "banana", origin: "ecuador", weight: 100},
+    {name: "mela", origin: "italia", weight: 200},
+    {name: "mango", origin: "venezuela", weight: 50},
+    {name: "avocado", origin: "perù", weight: 10},
+    {name: "papaya", origin: "guatemala", weight: 70},
+]
+
+const container = document.getElementById('fruit-container');
+
+for (const fruit of fruits) {
+    
+    const fruitHtml = `<div class="card">
+    <p>nome: ${fruit.name}</p>
+    <p>origin: ${fruit.origin}</p>
+    <p>peso: ${fruit.weight} kg</p>
+</div>`
+
+container.innerHTML += fruitHtml;
+
+}
